66b3d0e166f84943fb3a7d2d4ad0953902049b8b
arthur-leg <129770298+arthur-leg@users.noreply.github.com>
2023-05-25 14:19:47 +0200

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quotes")
+    print("2. Display quotes")
     print("3. Exit")
 
 def main():
@@ -15,11 +15,12 @@ def main():
         
         if choice == "1":
             print_quote(random_quote(quotes))
-        elif choice == "2":
-            view_quotes(quotes)
         elif choice == "3":
             print("Good bye...")
             break
+        elif choice == "2": # gestion de display_count()
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         else:
             print("Invalid input")
 
